642b4fb416040b80b546853346ec94c2ced5537a
Chris <cnicho14@wvup.edu>
2019-09-17 16:35:26 -0700

    abstracting out validation from model to employee controller

--- Models/EmployeeModel.cs
+++ Models/EmployeeModel.cs
@@ -1,74 +1,14 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.Text.RegularExpressions;
 
 namespace Zeit.Models
 {
     public class EmployeeModel
     {
         public Guid Id { get; }
-        private string _firstName;
-        private string _lastName;
-        private string _divison;
-        private bool _result;
-        public string firstName
-        {
-            get
-            {
-                return _firstName;
-            }
-            set
-            {
-                _result = Regex.IsMatch(firstName, @"^[a-zA-Z]+$");
-                if (!String.IsNullOrEmpty(firstName) && _result)
-                {
-                    this._firstName = firstName;
-                }
-                else
-                {
-                    throw new FormatException("First name can not be empty, must only contain letters.");
-                }
-            }
-        }
-        public string lastName
-        {
-            get
-            {
-                return _lastName;
-            }
-            set
-            {
-                _result = Regex.IsMatch(lastName, @"^[a-zA-Z]+$");
-                if (!String.IsNullOrEmpty(lastName) && _result)
-                {
-                    this._lastName = lastName;
-                }
-                else
-                {
-                    throw new FormatException("Last name can not be empty, must only contain letters.");
-                }
-            }
-        }
-        public string divison
-        {
-            get
-            {
-                return _divison; 
-            }
-            set
-            {
-                _result = Regex.IsMatch(divison, @"^[a-zA-Z]+$");
-                if (!String.IsNullOrEmpty(divison) && _result)
-
-                {
-                    this._divison = divison;
-                }
-                else
-                {
-                    throw new FormatException("Must provide divison name.");
-                }
-            }
-        }
+      
+        public string firstName{get; set;}
+        public string lastName{get; set;}
+        public string divison{get; set;}
         public bool exempt { get; set; }
     }
 }

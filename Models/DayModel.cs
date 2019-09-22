f9c8bf30db841dbd173db3753b852d9a0d0c1e6c
Chris <cnicho14@wvup.edu>
2019-09-21 17:38:05 -0700

    added id back to employee, added status message and approval status to dayModel

--- Models/DayModel.cs
+++ Models/DayModel.cs
@@ -5,9 +5,14 @@ namespace Zeit.Models
     public class DayModel
     {
         Guid DayId { get; }
-        DateTime Enter { get; set; }
+
+        DateTime Enter { get; set;}
 
         DateTime Exit { get; set; }
+        
+        string? statusMessage{get; set;}
+
+        bool? Approved{get; set;}
 
         float HoursWorked { get; set; }
     }   

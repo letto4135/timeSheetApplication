a46392a20b75bb034846d157124cd508b8edc842
AlexLife <letto4135@gmail.com>
2019-09-27 16:20:39 -0400

    dbcontext

---
+++ Data/Migrations/20190927201810_initial migration.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace timeSheetApplication.Data.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "divison",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "employeeID",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "exempt",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "firstName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "lastName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "TimeSheets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeSheets", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TimeSheets");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "divison",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "employeeID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "exempt",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "firstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "lastName",
+                table: "AspNetUsers");
+        }
+    }
+}
--- Models/TimeSheetModel.cs
+++ Models/TimeSheetModel.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Zeit.Models
 {
-    public class TimeSheet
+    public class TimeSheetModel
     {
         public Guid TimesheetId { get; }
 
